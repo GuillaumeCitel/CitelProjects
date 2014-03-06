81373de827b7008563461e22ae24547115172867
guillaume <guillaume.faure@citel2cp.com>
2014-03-06 12:53:34 +0100

    Ajout du dossier script

---
+++ Scripts/All_script_dump_and_view_creation.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+  
+  set -e
+  
+  pg_dump -U kettlede --clean kettlede > /tmp/kettlede
+  echo "dump kettlede done"
+  
+  pg_dump -U kettlecn --clean shanghai > /tmp/shanghai
+  echo "dump shanghai done"
+  
+  pg_dump -U kettlecn --clean ccsh > /tmp/ccsh
+  echo "dump ccsh done"
+  
+  pg_dump -U kettleus --clean us > /tmp/us
+  echo "dump us done"
+  
+  pg_dump -U kettle --clean kettle > /tmp/france
+  echo "dump france done"
+  
+  # We need to delete the view before restoration because otherwise, tables won't be deleted (--clean instrauctions won't work) and so records will be inserted twice
+  if [ "$(hostname)" == "pc-guillaume" ]; then
+    /home/guillaume/kettle_4.4.0/kitchen.sh /file:"/home/guillaume/git/CitelProjects/Kettle_Extraction/All/delete_views.kjb"
+  else
+    /home/guillaume/kettle_4.4.0/kitchen.sh /file:"/home/guillaume/apache-tomcat-7.0.41/webapps/citelstats/Reports/Kettle_Extraction/All/delete_views.kjb"
+  fi
+  
+  # We do the restoration as postgres (superuser) to be able to execute any instruction.
+  
+  sed -i 's/public/kettlede/' /tmp/kettlede
+  echo "sed  kettlede done"
+  psql -U postgres All < /tmp/kettlede
+  echo "restore shanghai done"
+  
+  sed -i 's/public/shanghai/' /tmp/shanghai
+  echo "sed  shanghai ok"
+  psql -U postgres All < /tmp/shanghai
+  echo "restore shanghai done"
+  
+  sed -i 's/public/ccsh/' /tmp/ccsh
+  echo "sed  ccsh ok"
+  psql -U postgres All < /tmp/ccsh
+  echo "restore ccsh done"
+  
+  sed -i 's/public/us/' /tmp/us
+  echo "sed  us ok"
+  psql -U postgres All < /tmp/us
+  echo "restore us done"
+  
+  sed -i 's/public/france/' /tmp/france
+  echo "sed  france ok"
+  psql -U postgres All < /tmp/france
+  echo "restore france done"
+  
+  #We recreate the views
+  if [ "$(hostname)" == "pc-guillaume" ]; then
+    /home/guillaume/kettle_4.4.0/kitchen.sh /file:"/home/guillaume/git/CitelProjects/Kettle_Extraction/All/create_views.kjb"
+  else
+    /home/guillaume/kettle_4.4.0/kitchen.sh /file:"/home/guillaume/apache-tomcat-7.0.41/webapps/citelstats/Reports/Kettle_Extraction/All/create_views.kjb"
+  fi
+  
+  
+  set +e
