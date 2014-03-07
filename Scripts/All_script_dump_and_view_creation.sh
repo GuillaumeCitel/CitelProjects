d6e2b22aed7b39d0f9ab2318e798b5f9d41ba2a4
guillaume <guillaume.faure@citel2cp.com>
2014-03-07 15:50:28 +0100

    utilisation de drop schema

--- Scripts/All_script_dump_and_view_creation.sh
+++ Scripts/All_script_dump_and_view_creation.sh
@@ -2,52 +2,64 @@
   
   set -e
   
-  pg_dump -U kettlede --clean kettlede > /tmp/kettlede
-  echo "dump kettlede done"
+  pg_dump -U kettlede kettlede > /tmp/kettlede_from_local
+  echo "dump kettlede_from_local done"
   
-  pg_dump -U kettlecn --clean shanghai > /tmp/shanghai
-  echo "dump shanghai done"
+  pg_dump -U kettlecn shanghai > /tmp/shanghai_from_local
+  echo "dump shanghai_from_local done"
   
-  pg_dump -U kettlecn --clean ccsh > /tmp/ccsh
-  echo "dump ccsh done"
+  pg_dump -U kettlecn ccsh > /tmp/ccsh_from_local
+  echo "dump ccsh_from_local done"
   
-  pg_dump -U kettleus --clean us > /tmp/us
-  echo "dump us done"
+  pg_dump -U kettleus us > /tmp/us_from_local
+  echo "dump us_from_local done"
   
-  pg_dump -U kettle --clean kettle > /tmp/france
-  echo "dump france done"
-  
-  # We need to delete the view before restoration because otherwise, tables won't be deleted (--clean instrauctions won't work) and so records will be inserted twice
-  /home/guillaume/kettle_4.4.0/kitchen.sh /file:"/home/guillaume/apache-tomcat-7.0.41/webapps/citelstats/Reports/Kettle_Extraction/All/delete_views.kjb"
+  pg_dump -U kettle kettle > /tmp/france_from_local
+  echo "dump france_from_local done"
   
   # We do the restoration as postgres (superuser) to be able to execute any instruction.
   
-  sed -i 's/public/kettlede/' /tmp/kettlede
-  echo "sed  kettlede done"
-  psql -U postgres All < /tmp/kettlede
-  echo "restore shanghai done"
+  cp /tmp/kettlede_from_local /tmp/kettlede_from_local_sed
+  sed -i 's/public/kettlede/' /tmp/kettlede_from_local_sed
+  echo "sed  kettlede_from_local done"
+  psql All -U postgres -c 'DROP SCHEMA IF EXISTS kettlede CASCADE'
+  psql All -U postgres -c 'CREATE SCHEMA kettlede'
+  psql All -U postgres < /tmp/kettlede_from_local_sed
+  echo "restore kettlede_from_local_sed done"
   
-  sed -i 's/public/shanghai/' /tmp/shanghai
-  echo "sed  shanghai ok"
-  psql -U postgres All < /tmp/shanghai
-  echo "restore shanghai done"
+  cp /tmp/shanghai_from_local /tmp/shanghai_from_local_sed
+  sed -i 's/public/shanghai/' /tmp/shanghai_from_local_sed
+  echo "sed  shanghai_from_local ok"
+  psql All -U postgres -c 'DROP SCHEMA IF EXISTS shanghai CASCADE'
+  psql All -U postgres -c 'CREATE SCHEMA shanghai'
+  psql All -U postgres < /tmp/shanghai_from_local_sed
+  echo "restore shanghai_from_local_sed done"
   
-  sed -i 's/public/ccsh/' /tmp/ccsh
-  echo "sed  ccsh ok"
-  psql -U postgres All < /tmp/ccsh
-  echo "restore ccsh done"
+  cp /tmp/ccsh_from_local /tmp/ccsh_from_local_sed
+  sed -i 's/public/ccsh/' /tmp/ccsh_from_local_sed
+  echo "sed  ccsh_from_local ok"
+  psql All -U postgres -c 'DROP SCHEMA IF EXISTS ccsh CASCADE'
+  psql All -U postgres -c 'CREATE SCHEMA ccsh'
+  psql All -U postgres < /tmp/ccsh_from_local_sed
+  echo "restore ccsh_from_local_sed done"
   
-  sed -i 's/public/us/' /tmp/us
-  echo "sed  us ok"
-  psql -U postgres All < /tmp/us
-  echo "restore us done"
+  cp /tmp/us_from_local /tmp/us_from_local_sed
+  sed -i 's/public/us/' /tmp/us_from_local_sed
+  echo "sed  us_from_local ok"
+  psql All -U postgres -c 'DROP SCHEMA IF EXISTS us CASCADE'
+  psql All -U postgres -c 'CREATE SCHEMA us'
+  psql All -U postgres < /tmp/us_from_local_sed
+  echo "restore us_from_local_sed done"
   
-  sed -i 's/public/france/' /tmp/france
-  echo "sed  france ok"
-  psql -U postgres All < /tmp/france
-  echo "restore france done"
+  cp /tmp/france_from_local /tmp/france_from_local_sed
+  sed -i 's/public/france/' /tmp/france_from_local_sed
+  echo "sed  france_from_local ok"
+  psql All -U postgres -c 'DROP SCHEMA IF EXISTS france CASCADE'
+  psql All -U postgres -c 'CREATE SCHEMA france'
+  psql All -U postgres < /tmp/france_from_local_sed
+  echo "restore france_from_local_sed done"
   
-  #We recreate the views
+  #We recreate the views that have been deleted by the drop cascade
   /home/guillaume/kettle_4.4.0/kitchen.sh /file:"/home/guillaume/apache-tomcat-7.0.41/webapps/citelstats/Reports/Kettle_Extraction/All/create_views.kjb"
   
   set +e
